40aa3a5712c6d8270d4c1861776ac38ec9489b87
SalmanFarshe <salmannfarshee@gmail.com>
2021-09-17 02:59:11 +0600

    i added the copy able version

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
 var button = document.querySelector(".button");
 function generatingRandomHexaColorCode() {
-	const body = document.querySelector("body");
 	const hexzlColorCode = "#" + Math.floor(Math.random() * 10000000).toString(16);
+	const body = document.querySelector("body");
+	const colorCode = document.querySelector(".colorCode");
 	const h1 = document.querySelector(".h1");
-    body.style.backgroundColor = `${hexzlColorCode}`;
-    button.innerHTML = `Change Background <br> ${hexzlColorCode}`
-    button.style.backgroundColor = `${hexzlColorCode}`
-    h1.style.color =  `${hexzlColorCode}`
-}
+	body.style.backgroundColor = `${hexzlColorCode}`;
+	colorCode.innerHTML = `<br>${hexzlColorCode}`;
+	button.style.backgroundColor = `${hexzlColorCode}`;
+	h1.style.color = `${hexzlColorCode}`;
+	// copy color code
+	const inputele = document.createElement("input");
+	inputele.value = `${hexzlColorCode}`;
+	document.body.appendChild(inputele);
+	inputele.select();
+	document.execCommand("copy");
+	document.body.removeChild(inputele);
 
+	var alerrt = document.querySelector(".alerrt");
+	alerrt.style.display = "block";
+    setTimeout(() => {
+        alerrt.style.display = "none"
+    }, 1500);
+}
 button.addEventListener("click", generatingRandomHexaColorCode);
